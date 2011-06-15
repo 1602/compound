1fc95ead6db173dcc2b4b65dc55fd350c8fbb352
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.ru>
2011-06-15 19:13:25 +0400

    Removed mongo sesion storage on initial setup

--- templates/mongoose/config/environment.coffee
+++ templates/mongoose/config/environment.coffee
@@ -1,25 +1,4 @@
-mongoStore = require 'connect-mongodb'
-express    = require 'express'
-
-if process.env.MONGOHQ_URL
-    app.settings.mongoUrl = process.env.MONGOHQ_URL
-    m = require('url').parse process.env.MONGOHQ_URL
-    app.settings.db =
-        driver:   'mongoose'
-        database: m.pathname.replace(/^\//, '')
-        port:     m.port
-        host:     m.hostname
-        user:     m.auth.split(':')[0]
-        password: m.auth.split(':')[1]
-else
-    app.settings.db = JSON.parse(require('fs').readFileSync(__dirname + '/database.json', 'utf-8'))[app.settings.env]
-
-mongoSessionStore = new mongoStore
-    dbname:   app.settings.db.database
-    host:     app.settings.db.host
-    port:     app.settings.db.port
-    username: app.settings.db.user
-    password: app.settings.db.password
+express = require 'express'
 
 app.configure ->
     cwd = process.cwd()
@@ -30,7 +9,7 @@ app.configure ->
     app.use express.static(cwd + '/public', maxAge: 86400000)
     app.use express.bodyParser()
     app.use express.cookieParser()
-    app.use express.session secret: 'secret', store: mongoSessionStore
+    app.use express.session secret: 'secret'
     app.use express.methodOverride()
     app.use app.router
 
--- templates/mongoose/config/environment.js
+++ templates/mongoose/config/environment.js
@@ -1,31 +1,4 @@
 var express    = require('express');
-var mongoStore = require('connect-mongodb');
-
-if (process.env.MONGOHQ_URL) {
-    // save to app settings
-    app.settings.mongoUrl = process.env.MONGOHQ_URL;
-    // save to db settings
-    var m = require('url').parse(process.env.MONGOHQ_URL);
-    app.settings.db = {
-        driver:   'mongoose',
-        database: m.pathname.replace(/^\//, ''),
-        port:     m.port,
-        host:     m.hostname,
-        user:     m.auth.split(':')[0],
-        password: m.auth.split(':')[1]
-    };
-} else {
-    app.settings.db = JSON.parse(require('fs').readFileSync(__dirname + '/database.json', 'utf-8'))[app.settings.env];
-}
-
-// connect with db for session store
-var mongoSessionStore = new mongoStore({
-    dbname:   app.settings.db.database,
-    host:     app.settings.db.host,
-    port:     app.settings.db.port,
-    username: app.settings.db.user,
-    password: app.settings.db.password
-}, function () {});
 
 app.configure(function(){
     var cwd = process.cwd();
@@ -34,7 +7,7 @@ app.configure(function(){
     app.set('view engine', 'VIEWENGINE');
     app.use(express.bodyParser());
     app.use(express.cookieParser());
-    app.use(express.session({secret: 'secret', store: mongoSessionStore}));
+    app.use(express.session({secret: 'secret'}));
     app.use(express.methodOverride());
     app.use(app.router);
 });
