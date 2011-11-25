eaa386b127b99de9f7b02ee8d97a007a9986955b
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2011-11-25 13:11:57 +0700

    Generic routes in template generator

---
+++ templates/config/routes.js
@@ -0,0 +1,9 @@
+exports.routes = function (map) {
+
+
+
+    // Generic routes. Add all your routes below this line
+    // feel free to remove generic routes
+    map.all(':controller/:action');
+    map.all(':controller/:action/:id');
+};
