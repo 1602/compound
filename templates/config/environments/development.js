dbc7aabc34395f3e148126247987739593780e6e
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2012-10-23 17:28:52 +0400

    Cleaning up old code and global variables

--- templates/config/environments/development.js
+++ templates/config/environments/development.js
@@ -1,9 +1,7 @@
 app.configure('development', function () {
-    app.disable('view cache');
-    app.disable('model cache');
-    app.disable('eval cache');
     app.enable('log actions');
     app.enable('env info');
+    app.enable('watch');
     app.use(require('express').errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
--- templates/config/environments/production.js
+++ templates/config/environments/production.js
@@ -1,7 +1,4 @@
 app.configure('production', function () {
-    app.enable('view cache');
-    app.enable('model cache');
-    app.enable('eval cache');
     app.enable('merge javascripts');
     app.enable('merge stylesheets');
     app.disable('assets timestamps');
