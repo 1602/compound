64fade4dd0d407750f4207e081986ad6e9bdcc39
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2012-10-30 03:58:11 +0400

    Params/body logging, backward compat in CLI, camelcase

--- templates/server.js
+++ templates/server.js
@@ -7,6 +7,7 @@
  * @returns RailwayJS powered express webserver
  */
 var app = module.exports = function getServerInstance(params) {
+    params = params || {};
     // specify current dir as default root of server
     params.root = params.root || __dirname;
     return require('railway').createServer(params);
