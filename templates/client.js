c494a498cfaf797fddd8c5d24ec160faa65b040c
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-02-24 03:34:04 +0700

    Browserify v2 support

--- templates/client.js
+++ templates/client.js
@@ -1,34 +1,3 @@
-require.define('module', function () {});
-require.define('fs', function(require, module, exports) {
-    var path = require('path');
-    exports.readFileSync = function (path) {
-        return compound.files[path];
-    };
-
-    exports.existsSync = function (name) {
-        if (name in compound.files) return true;
-        var dirFound = false;
-        Object.keys(compound.files).forEach(function (dir) {
-            if (path.dirname(dir).indexOf(name) !== -1) {
-                dirFound = true;
-            }
-        });
-        return dirFound;
-    };
-
-    exports.readdirSync = function (name) {
-        var res = [];
-        name = name.replace(/\/$/, '');
-        Object.keys(compound.files).forEach(function (file) {
-            if (path.dirname(file) === name) {
-                res.push(path.basename(file));
-            }
-        });
-        return res;
-    };
-});
-
-require('./node_modules/compound/lib/client/application');
 var CompoundClient = require('./node_modules/compound/lib/client/compound');
 var express = require('express');
 
@@ -38,16 +7,16 @@ var compound = window.compound = new CompoundClient(app, '{{ ROOT }}');
 
 var fs = require('fs');
 
-compound.files = {
-    {{ FILES }}
-};
+window.files = window.files || {};
+{{ FILES }}
 
 {{ ENGINES }}
 
-Object.keys(compound.files).forEach(function (f) {
+Object.keys(window.files).forEach(function (f) {
+    return;
     if (f.match(/\.js$/)) {
         require.define(f, new Function('require', 'module', 'exports',
-        '__dirname', '__filename', 'process', 'global', compound.files[f]));
+        '__dirname', '__filename', 'process', 'global', window.files[f]));
     }
 });
 
@@ -84,4 +53,6 @@ function initializeBrowser() {
 }
 
 $(initializeBrowser);
+
+compound.init();
 
