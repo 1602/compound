b641aeff495fa712c14d1473a383c07df18e27d3
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-02-25 04:03:05 +0700

    Prepare for multi-modular apps

--- templates/client.js
+++ templates/client.js
@@ -1,84 +1,86 @@
-var CompoundClient = require('./node_modules/compound/lib/client/compound');
 var express = require('express');
 var util = require('util');
 var fs = require('fs');
+var CompoundClient = require('compound/lib/client/compound');
 
-var app = express();
-app.set('env', '{{ NODE_ENV }}');
-var compound = window.compound = new CompoundClient(app, '{{ ROOT }}');
+module.exports = instantiateApplication;
 
-var fs = require('fs');
+if (!window.compound) {
+    window.compound = instantiateApplication('{{ ROOT }}');
 
-window.files = window.files || {};
-{{ FILES }}
+    $(function initializeBrowser() {
+        $('a').live('click', compound.app.handle);
+        $('form').live('submit', compound.app.handle);
+        var skipFirst = true;
 
-{{ ENGINES }}
-
-Object.keys(window.files).forEach(function (f) {
-    return;
-    if (f.match(/\.js$/)) {
-        require.define(f, new Function('require', 'module', 'exports',
-        '__dirname', '__filename', 'process', 'global', window.files[f]));
-    }
-});
-
-compound.structure = function () {
-    return {
-        controllers: {
-            {{ CONTROLLERS }}
-        },
-        helpers: {
-            {{ HELPERS }}
-        },
-        models: {
-            {{ MODELS }}
-        },
-        views: {
-            {{ VIEWS }}
-        }
-    };
-};
-
-compound.on('configure', function () {
-    console.log('configure');
-    [
-        require('./config/environment'),
-        require('./config/environments/{{ NODE_ENV }}')
-    ].forEach(function (m) {
-        if (m && m.call) m(compound);
-    });
-}).on('routes', function (map) {
-    var r = require('./config/routes');
-    if (r && r.routes) r.routes(map)
-}).on('extensions', function (c) {
-    var f = require('./config/autoload');
-    if (typeof f === 'function') {
-        var s = f(compound);
-        s && s.forEach && s.forEach(function (m) {
-            m && m.init && m.init(c);
+        $(window).bind('popstate', function () {
+            if (skipFirst) {
+                skipFirst = false;
+                return;
+            }
+            compound.app.handle(location.pathname, true);
         });
-    }
-}).on('initializers', function (c) {
-    [{{ INITIALIZERS }}].forEach(function (m) {
-        m(c);
+
     });
-});
-
-function initializeBrowser() {
-    $('a').live('click', compound.app.handle);
-    $('form').live('submit', compound.app.handle);
-    var skipFirst = true;
-
-    $(window).bind('popstate', function () {
-        if (skipFirst) {
-            skipFirst = false;
-            return;
-        }
-        compound.app.handle(location.pathname, true);
-    });
-
 }
 
-$(initializeBrowser);
+function instantiateApplication(root) {
 
-compound.init();
+    var app = express();
+    app.set('env', '{{ NODE_ENV }}');
+    var compound = window.compound = new CompoundClient(app, root);
+
+    var fs = require('fs');
+
+    window.files = window.files || {};
+
+    {{ FILES }}
+
+    {{ ENGINES }}
+
+    compound.structure = function () {
+        return {
+            controllers: {
+                {{ CONTROLLERS }}
+            },
+            helpers: {
+                {{ HELPERS }}
+            },
+            models: {
+                {{ MODELS }}
+            },
+            views: {
+                {{ VIEWS }}
+            }
+        };
+    };
+
+    compound.on('configure', function () {
+        [
+            require('./config/environment'),
+            require('./config/environments/{{ NODE_ENV }}')
+        ].forEach(function (m) {
+            if (m && m.call) m(compound);
+        });
+    }).on('routes', function (map) {
+        var r = require('./config/routes');
+        if (r && r.routes) r.routes(map)
+    }).on('extensions', function (c) {
+        var f = require('./config/autoload');
+        if (typeof f === 'function') {
+            var s = f(compound);
+            s && s.forEach && s.forEach(function (m) {
+                m && m.init && m.init(c);
+            });
+        }
+    }).on('initializers', function (c) {
+        [{{ INITIALIZERS }}].forEach(function (m) {
+            m(c);
+        });
+    });
+
+    compound.init();
+
+    return compound;
+
+};
