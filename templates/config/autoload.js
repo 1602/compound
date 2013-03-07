437fc81222fb7a61d8a86b6267852e22303b5bdc
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-03-07 18:54:08 +0800

    Fix autoload

--- templates/config/autoload.js
+++ templates/config/autoload.js
@@ -2,7 +2,7 @@ module.exports = function (compound) {
     return typeof window === 'undefined' ? [
         'jugglingdb',
         'co-assets-compiler'
-    ].concat(compiler.app.get('env') === 'development' ? [
+    ].concat('development' == compound.app.get('env') ? [
         '{{ VIEWENGINE }}-ext',
         'seedjs',
         'co-generators',
