70a634a6e6b995b284fc1864ccf6a1b8ac26d6d6
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-04-02 02:02:27 +0400

    Compound model API
    - compound.model('modelname') - getter (case insensitive)
    - compound.model('ModelName', true) - case sensitive getter
    - compound.model(NamedFn) - setter for model as named fn (or jugglingdb model)

--- test/compound.test.js
+++ test/compound.test.js
@@ -1,4 +1,5 @@
 var Compound = require('../').Compound;
+var should = require('should');
 
 describe('Compound', function() {
 
@@ -12,6 +13,32 @@ describe('Compound', function() {
         var c = new Compound;
         c.generators.init(c);
         c.generators.list().should.equal('app');
+    });
+
+    it('should allow to find model by name', function() {
+        var c = new Compound;
+        c.models = {Model: function Model() {}, ModelTwo: true};
+        should.exist(c.model('Model'));
+        should.exist(c.model('model'));
+        should.not.exist(c.model('model', true));
+        should.exist(c.model('ModelTwo'), true);
+
+        // add model as named fn
+        should.not.exist(c.model('ModelThree'));
+        c.model(function ModelThree(){});
+        should.exist(c.model('ModelThree'));
+
+        // add model as jugglingdb model
+        var model = function(){};
+        model.modelName = 'HelloJuggling';
+        c.model(model);
+        should.exist(c.model('HelloJuggling'));
+
+        // throws when model is not named
+        (function() {
+            c.model(function() {});
+        }).should.throw('Named function or jugglingdb model required');
+
     });
 
 });
